936cf5876ee1759540a71d8404e929b126697cdf
fengli <954751186@qq.com>
2019-01-25 18:19:18 +0800

    分页

--- FLMusicServer/settings.py
+++ FLMusicServer/settings.py
@@ -137,6 +137,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4
 }
 
 AUTHENTICATION_BACKENDS = (
