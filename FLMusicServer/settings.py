7296b8389729026c9d6626e48456013901fc21d9
fengli <954751186@qq.com>
2019-02-01 16:34:52 +0800

    音乐列表 分页功能

--- FLMusicServer/settings.py
+++ FLMusicServer/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 4
+    'PAGE_SIZE': 4,
 }
 
 AUTHENTICATION_BACKENDS = (
--- apps/musics/views.py
+++ apps/musics/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
-from rest_framework import mixins, viewsets
 
+from base64 import b64encode
+
+from rest_framework import mixins, viewsets
+from rest_framework.pagination import CursorPagination
+from django.utils.six.moves.urllib import parse as urlparse
 from .models import Music
 from .serializers import MusicSerializer
 
 # Create your views here.
+
+class MusicPagination(CursorPagination):
+    ordering = "-add_time"
+    def encode_cursor(self, cursor):
+        """
+        Given a Cursor instance, return an url with encoded cursor.
+        """
+        tokens = {}
+        if cursor.offset != 0:
+            tokens['o'] = str(cursor.offset)
+        if cursor.reverse:
+            tokens['r'] = '1'
+        if cursor.position is not None:
+            tokens['p'] = cursor.position
+
+        querystring = urlparse.urlencode(tokens, doseq=True)
+        encoded = b64encode(querystring.encode('ascii')).decode('ascii')
+        return encoded
+
 class musicViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
+    pagination_class = MusicPagination
+
